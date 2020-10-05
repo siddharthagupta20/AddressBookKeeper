eefd230c8fbb786bbd78a3842aac88fef6307f46
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-10-05 17:06:20 +0530

    [Siddhartha] modified: condition for updating contact

--- src/main/java/com/cg/adp/AddressBookExecutor.java
+++ src/main/java/com/cg/adp/AddressBookExecutor.java
@@ -2,6 +2,8 @@ package com.cg.adp;
 
 import java.util.Scanner;
 
+import com.cg.adp.dto.AddressBook;
+import com.cg.adp.dto.PersonContact;
 import com.cg.adp.service.PersonContactService;
 
 public class AddressBookExecutor {
@@ -14,19 +16,41 @@ public class AddressBookExecutor {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program: ");
-		AddressBookExecutor abe=new AddressBookExecutor();
+		AddressBookExecutor abe = new AddressBookExecutor();
+		PersonContactService pcService = new PersonContactService();
+		AddressBook ab = new AddressBook();
 		boolean computation = true;
 		while (computation) {
-			System.out.println("Adding a contact in an address book: ");
-			PersonContactService pcService = new PersonContactService();
-			pcService.addContact();
+			System.out.println("1.Add contact. ");
+			System.out.println("2.Edit Contact.");
+			int option = abe.sc.nextInt();
+
+			switch (option) {
+			case 1:
+				ab.getAddressBook().add(pcService.addContact());
+				break;
+			case 2:
+				String firstName = pcService.gettingFirstName();
+				int y = 0;
+				for (PersonContact x : ab.getAddressBook()) {
+					if (x.getFirstName().equalsIgnoreCase(firstName)) {
+						break;
+					}
+					y++;
+				}
+				pcService.updateContact(ab.getAddressBook().get(y));
+				break;
+			default:
+				break;
+			}
 			System.out.println("Want to add more?(Y/N)");
 			char c = abe.sc.next().charAt(0);
 			if (c == 'y' || c == 'Y')
 				continue;
 			else
 				computation = false;
-		}
-	}
 
+		}
+
+	}
 }
--- src/main/java/com/cg/adp/dto/AddressBook.java
+++ src/main/java/com/cg/adp/dto/AddressBook.java
@@ -1,5 +1,32 @@
 package com.cg.adp.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
+
+	private String aname;
+	private List<PersonContact> addressBook;
+
+	public AddressBook() {
+		aname = "";
+		addressBook = new ArrayList<PersonContact>();
+	}
+
+	public void setAddressBook(List<PersonContact> addressBook) {
+		this.addressBook = addressBook;
+	}
+
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+
+	public List<PersonContact> getAddressBook() {
+		return addressBook;
+	}
+
+	public String getAname() {
+		return aname;
+	}
 
 }
