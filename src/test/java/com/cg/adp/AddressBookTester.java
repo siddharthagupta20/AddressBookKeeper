4b793e627b3672aef10f0b49684cecaafb7e5117
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-10-19 15:04:59 +0530

    [Siddhartha] refactored: reading and writing to CSV files

--- src/test/java/com/cg/adp/AddressBookTester.java
+++ src/test/java/com/cg/adp/AddressBookTester.java
@@ -1,5 +1,6 @@
 package com.cg.adp;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.nio.file.Files;
@@ -10,18 +11,22 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.cg.adp.dto.AddressBook;
 import com.cg.adp.dto.PersonContact;
 
 public class AddressBookTester {
 
 	public static final String AB1 = "AB1.txt";
+	public static final String AB2 = "AB2.txt";
+	public static final String AB3 = "AB3.csv";
+	public static final String AB4 = "AB4.csv";
 
 	@Test
 	public void givenAddressBookFileshouldRead() {
 		Path p = Paths.get(AB1);
-		List<PersonContact> ab=new ArrayList<>();
+		List<PersonContact> ab = new ArrayList<>();
 		try {
-			ab=AddressBookFileIOService.readFile(p);
+			ab = AddressBookFileIOService.readFile(p);
 		} catch (PersonContactException e) {
 			System.out.println(e.getMessage());
 		}
@@ -29,12 +34,42 @@ public class AddressBookTester {
 		assertTrue(Files.exists(p));
 
 	}
+
 	@Test
 	public void givenPersonContactShouldWriteOnFile() {
-		Path p=Paths.get(AB1);
-		PersonContact personToWrite=new PersonContact("The","Weeknd","DontKnow","Cali","SomeState",666666l,"00 9999999999","weeknd.420@gmail.com");
+		Path p = Paths.get(AB2);
+		PersonContact personToWrite = new PersonContact("The", "Weeknd", "DontKnow", "Cali", "SomeState", 666666l,
+				"00 9999999999", "weeknd.420@gmail.com");
 		AddressBookFileIOService.writeToFile(p, personToWrite);
 		assertTrue(true);
 	}
+
+	@Test
+	public void givenAddressBookCSVFileShoudRead() {
+		Path p = Paths.get(AB3);
+		List<PersonContact> addressBookRead = AddressBookCSVIOService.readCSV(p);
+		System.out.println(addressBookRead);
+		assertEquals("Donald", addressBookRead.get(0).getFirstName());
+
+	}
+
+	@Test
+	public void givenAddressBookShouldWriteOnCSVFile() {
+		Path p = Paths.get(AB4);
+		PersonContact personToWrite = new PersonContact("The", "Weeknd", "DontKnow", "Cali", "SomeState", 666666l,
+				"00 9999999999", "weeknd.420@gmail.com");
+		PersonContact personToWrite2 = new PersonContact("Gerard", "Eazy", "DontKnow", "Cali", "SomeState", 666666l,
+				"00 9999999999", "weeknd.420@gmail.com");
+		List<PersonContact> addressBookWrite = new ArrayList<>();
+		addressBookWrite.add(personToWrite);
+		addressBookWrite.add(personToWrite2);
+		AddressBook abObj = new AddressBook();
+		abObj.setAddressBook(addressBookWrite);
+		AddressBookCSVIOService.writeCSV(p, abObj);
+		List<PersonContact> addressBookRead = AddressBookCSVIOService.readCSV(p);
+		System.out.println(addressBookRead);
+		assertEquals("The", addressBookRead.get(0).getFirstName());
+	
+		}
 
 }
